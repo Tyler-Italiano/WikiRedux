009d8096ea004dddaa4bd1f03537764882f50f82
Tyler Italiano <tylerjitaliano@gmail.com>
2017-01-23 20:43:32 -0600

    Setup

---
+++ Main.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  WikiRedux
+//
+//  Created by Tyler Italiano on 1/23/17.
+//  Copyright © 2017 PrimitiveSoftware. All rights reserved.
+//
+
+import UIKit
+
+class Main: UIViewController {
+    @IBOutlet weak var easyButtonOutlet: UIButton!
+    @IBOutlet weak var hardButtonOutlet: UIButton!
+    @IBOutlet weak var nearlyImpossibleButtonOutlet: UIButton!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
